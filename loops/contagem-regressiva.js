4aac1c2ad58b5317725eb41e9459d43a25462546
Mourão <p3dr0sk0sk7160@gmail.com>
2026-01-16 17:26:29 -0300

    adiciona script de contagem regressiva de 10 a 0 com mensagem final

---
+++ loops/contagem-regressiva.js
@@ -0,0 +1,19 @@
+// Você é uma pessoa desenvolvedora de interface em uma startup de tecnologia aeroespacial e precisa criar uma contagem regressiva automática para o painel de lançamento. A contagem deve começar de 10 até 0, exibindo “Lançar!” ao final.
+
+// Crie um programa que conte de 10 até 0 e exiba a mensagem “Lançar!” ao final.
+
+const numeroFinal = 10;
+gerarContagem(numeroFinal);
+
+function gerarContagem(numeroFinal) {
+    for (let contador = numeroFinal; contador >= 0; contador--) {
+        setTimeout(() => {
+            console.log(contador);
+        }, (numeroFinal - contador) * 1000);
+    }
+    if (numeroFinal >= 0) {
+        setTimeout(() => {
+            console.log("Lançar!");
+        }, (numeroFinal + 1) * 1000);
+    }
+}
