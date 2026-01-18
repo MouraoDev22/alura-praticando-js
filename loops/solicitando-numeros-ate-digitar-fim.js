a67df95e982d697dc32625ef01106c8a24bf1d34
Mourão <p3dr0sk0sk7160@gmail.com>
2026-01-18 02:46:39 -0300

    adiciona script para cadastro e exibição de nomes até a palavra "fim"

---
+++ loops/solicitando-numeros-ate-digitar-fim.js
@@ -0,0 +1,33 @@
+// Você está criando um sistema de cadastro em que a pessoa usuária pode cadastrar quantos nomes quiser, um por vez. O processo deve continuar até que seja encontrado a palavra "fim" (com letras minúsculas).
+
+// Crie um programa que com base em uma entrada pré-definida de nomes, exiba cada um deles, e encerre somente quando encontrar o valor "fim".
+
+let listaDeNomes = [];
+const regex = /^[A-Za-zÀ-ú\s]+$/;
+const palavraFim = "fim";
+
+inserirNomes();
+listarNomes(listaDeNomes);
+
+function inserirNomes() {
+    for (let i = 0; i < 9; i++) {
+        let nome = prompt("Digite um nome:");
+        while (!regex.test(nome)) {
+            nome = prompt("Entrada inválida. Por favor, digite um nome válido:");
+        }
+        listaDeNomes.push(nome);
+    }
+    const nomeFinal = listaDeNomes[6];
+
+    listaDeNomes.push(nomeFinal);
+    listaDeNomes[7] = palavraFim;
+}
+
+function listarNomes(nomes) {
+    for (let i = 0; i < nomes.length; i++) {
+        if (nomes[i] === "fim") {
+            break;
+        }
+        console.log(nomes[i]);
+    }
+}
