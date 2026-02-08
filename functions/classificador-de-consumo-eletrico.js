37b5bb0624f875da11ceb54fd040c6c6c45ce32f
Mourão <p3dr0sk0sk7160@gmail.com>
2026-02-08 14:55:21 -0300

    classificador de consumo concluído

--- functions/classificador-de-consumo-eletrico.js
+++ functions/classificador-de-consumo-eletrico.js
@@ -1,3 +1,11 @@
+// Você está desenvolvendo um sistema para ajudar pessoas a entenderem melhor o consumo de energia elétrica de seus aparelhos eletrônicos. O sistema deve calcular o consumo mensal estimado com base no uso diário, classificar o consumo (baixo, moderado ou alto) e exibir uma mensagem clara para o usuário.
+
+// Sua missão é criar três funções separadas, com responsabilidades bem definidas:
+
+// . calcularConsumo(potencia, horasPorDia): Retorna o consumo mensal em kWh, com base na fórmula: consumo = (potencia × horasPorDia × 30) / 1000
+// . classificarConsumo(consumo): Retorna a classificação com base na tabela:
+// . exibirResumo(nomeAparelho, consumo, classificacao): Exibe uma mensagem como:"Geladeira tem consumo de 180 kWh/mês e é classificada como Consumo moderado."
+
 const aparelhos = ['Geladeira', 'Microondas', 'Fogão', 'Máquina de Lavar', 'Lava Louças', 'Televisão', 'Ar Condicionado', 'Computador'];
 const nomeAparelho = aparelhos[Math.floor(Math.random() * aparelhos.length)];
 const consumo = calcularConsumo(150, 4);
