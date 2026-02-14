7911a1754d68ef4122b948c98beb38447b45cb4e
Mourão <p3dr0sk0sk7160@gmail.com>
2026-02-14 00:25:04 -0300

    feito script de acessar e modificar elementos da lista

---
+++ arrays/acessando-e-modificando-elementos-de-um-array.js
@@ -0,0 +1,10 @@
+import prompt from 'awt-prompt';
+
+const listaDeCompras = [ 
+    {nome: 'Arroz', quantidade: 1, unidade: 'kg'},
+    {nome: 'Feijão', quantidade: 1, unidade: 'kg'},
+    {nome: 'Carne', quantidade: 500, unidade: 'g'},
+    {nome: 'Leite', quantidade: 1, unidade: 'litro'},
+    {nome: 'Ovos', quantidade: 12, unidade: 'unidades'}
+];
+
