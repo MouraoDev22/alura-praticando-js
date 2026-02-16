c52f61d7191503ef5ce8a770d127a25641e383e2
Mourão <p3dr0sk0sk7160@gmail.com>
2026-02-16 20:47:19 -0300

    desconto.js concluído

--- arrays/busca-livros.js
+++ arrays/busca-livros.js
@@ -1,3 +1,10 @@
+// Você está desenvolvendo um sistema de busca para uma biblioteca digital. Quando a pessoa usuária digita o nome de um livro, o sistema precisa verificar se ele está disponível na lista de títulos cadastrados. Escreva um programa que:
+
+// . Crie um array com nomes de livros.
+// . Crie uma variável com o nome de um livro procurado.
+// . Verifique se o livro está na lista.
+// . Exiba uma mensagem informando se o livro foi encontrado ou não.
+
 import prompt from 'awt-prompt';
 
 const nomesDeLivros = ['Dom Casmurro', 'O Cortiço', 'Capitães da Areia', 'Iracema', 'Dom Quixote'];
---
+++ arrays/desconto.js
@@ -0,0 +1,26 @@
+// Você está desenvolvendo um sistema de pagamento e todos os produtos precisam receber um desconto de 10% antes de serem exibidos para as pessoas. Escreva um programa que:
+
+// . Crie um array com os preços originais dos produtos.
+// , Use o método map() para aplicar 10% de desconto em cada item.
+// . Exiba o novo array com os preços já com o desconto aplicado.
+
+const precos = [100, 80, 50, 120, 90];
+
+exibirPrecos(precos);
+
+function exibirPrecos(precos) {
+    console.log('\nPreços originais:\n');
+    precos.forEach((preco, index) => {
+        console.log(`${index + 1}. R$${preco.toFixed(2)}`);
+    });
+    
+    const precosComDesconto = aplicarDesconto(precos);
+    console.log('\nPreços com desconto de 10%:\n');
+    precosComDesconto.forEach((preco, index) => {
+        console.log(`${index + 1}. R$${preco.toFixed(2)}`);
+    });
+    console.log('');
+    return;
+};
+
+function aplicarDesconto(precos) { return precos.map(preco => preco - (preco * 0.1)) };
