3f12e0982aa85f5b0c25f02c5beedf5b9f028a74
Mourão <p3dr0sk0sk7160@gmail.com>
2026-02-15 22:59:30 -0300

    busca-livros.js concluído

---
+++ arrays/busca-livros.js
@@ -0,0 +1,44 @@
+import prompt from 'awt-prompt';
+
+const nomesDeLivros = ['Dom Casmurro', 'O Cortiço', 'Capitães da Areia', 'Iracema', 'Dom Quixote'];
+const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9.-\s]+$/;
+
+await buscarLivro(nomesDeLivros);
+
+async function buscarLivro(livros) {
+    console.log('\nDigite o nome do livro que deseja buscar:\n');
+    let nomeDoLivro = await prompt('> ');
+    
+    if (!regex.test(nomeDoLivro) || !nomeDoLivro.trim()) {
+        console.log('\nEntrada inválida. Por favor, digite apenas letras, pontos, hífens, números e espaços.\n');
+        return await buscarLivro(livros);
+    };
+
+    for (const livro of livros) {
+        if (livro.toLowerCase() === nomeDoLivro.toLowerCase()) {
+            console.log(`\nLivro "${livro}" encontrado!\n`);
+            console.log('Deseja buscar outro livro? (s/n)\n');
+            let resposta = await prompt('> ');
+            resposta = resposta.trim().toLowerCase();
+
+            if (resposta === 's') {
+                return buscarLivro(livros);
+            } else {
+                console.log('\nPrograma encerrado.\n');
+                return;
+            };
+        };
+    };
+
+    console.log(`\nLivro "${nomeDoLivro}" não encontrado.\n`);
+    console.log('Deseja buscar outro livro? (s/n)\n');
+    let resposta = await prompt('> ');
+    resposta = resposta.toLowerCase();
+            
+    if (resposta === 's') {
+         return await buscarLivro(livros);
+    } else {
+        console.log('\nPrograma encerrado.\n');
+        return;
+    };    
+};
--- arrays/lista-pedidos.js
+++ arrays/lista-pedidos.js
@@ -1,3 +1,10 @@
+// Você está desenvolvendo um sistema de pedidos online. Em certo momento, o sistema precisa criar uma cópia da lista de pedidos para simular alterações, sem modificar o array original. Escreva um programa que:
+
+// . Crie um array com alguns pedidos.
+// . Faça uma cópia do array.
+// . Adicione um novo item apenas na cópia.
+// . Exiba os dois arrays para mostrar que o original não foi alterado.
+
 import prompt from 'awt-prompt';
 
 const pedidos = [
