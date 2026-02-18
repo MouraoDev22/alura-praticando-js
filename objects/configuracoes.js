10e6cfaac6571c84ac018ec3c6c43f53d8781743
Mourão <p3dr0sk0sk7160@gmail.com>
2026-02-18 20:34:16 -0300

    configuracoes.js concluído

---
+++ objects/configuracoes.js
@@ -0,0 +1,27 @@
+// Você está criando um painel de configurações para um aplicativo. As configurações são armazenadas em um objeto e você precisa acessar informações específicas para exibição.
+
+// Escreva um programa que:
+
+// . Crie um objeto configuracoes com as propriedades de tema, notificacoes, idioma.
+// . Acesse tema usando notação de ponto.
+// . Acesse idioma usando notação de colchetes.
+// . Exiba os dois valores no console.
+
+const configuracoes = {
+    tema: 'dark',
+    idioma: 'pt-BR',
+    notificacoes: true,
+    exibirConfig: function(...keys) {
+        console.log('');
+        for (const key of keys) {
+            if (this.hasOwnProperty(key)) {
+                console.log(`${key[0].toUpperCase() + key.slice(1)}: ${this[key]}`);
+            } else {
+                console.log(`\n"${key}" não existe nas configurações.\n`);
+            };
+        };
+        console.log('');
+    },
+};
+
+configuracoes.exibirConfig('tema', 'idioma');
